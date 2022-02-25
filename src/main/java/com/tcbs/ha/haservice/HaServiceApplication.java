627384118efa6a3572c03ae55fc8264975d916d2
Nguyễn Tường Quân <quannt3@tcbs.com.vn>
2022-02-25 15:25:45 +0700

    initial

---
+++ src/main/java/com/tcbs/ha/haservice/HaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tcbs.ha.haservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HaServiceApplication.class, args);
+  }
+
+}
